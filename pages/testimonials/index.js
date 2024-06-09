61f0539ef484fd329eb1c8ff6b50c6212adf2124
sergeishay <sergeishay@gmail.com>
2024-06-09 12:47:20 +0300

    testamonials update

--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -1,41 +1,14 @@
 'use client';
-import { HeroParallax } from "../../components/ui/hero-parallax";
-import { getProjects } from "../../dataConnector";
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 
 
 
 const Testimonials =  () => {
-  
-  const [projects, setProjects] = useState([]); // State to store fetched projects
-
-  useEffect(() => {
-    const fetchProjects = async () => {
-      try {
-        const { data } = await getProjects(); // Await the Promise from getProjects
-        // Transform the fetched data into the required format
-        const formattedProjects = data.projects.edges.map(({ node }) => ({
-          title: node.title.replace(/ /g, '-'),
-          link: node.liveLink,
-          thumbnail: node.mainImage,
-          description: node.description,
-          gallery: node.gallery,
-          id: node.id,
-        }));
-        setProjects(formattedProjects); // Update state with formatted data
-      } catch (error) {
-        console.error("Error fetching projects:", error);
-      }
-    };
-
-    fetchProjects(); // Invoke the async function to fetch and process projects
-  }, []); 
-  console.log(projects);
 
   return (
     <div>
-      <HeroParallax products={projects} />
+
     </div>
   );
 };
